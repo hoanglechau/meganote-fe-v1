d22d5dc1ca7adc85a80f0fc1cad1ff4bc1bcbe9d
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-05-17 15:24:45 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,13 @@ import Public from "./components/Public";
 import Login from "./features/auth/Login";
 import DashLayout from "./components/DashLayout";
 import Welcome from "./features/auth/Welcome";
-import NoteList from "./features/notes/NoteList";
-import UserList from "./features/users/UserList";
-import { store } from "./app/store";
-import { Provider } from "react-redux";
+import NotesList from "./features/notes/NotesList";
+import UsersList from "./features/users/UsersList";
+import EditUser from "./features/users/EditUser";
+import NewUserForm from "./features/users/NewUserForm";
+import EditNote from "./features/notes/EditNote";
+import NewNote from "./features/notes/NewNote";
+import Prefetch from "./features/auth/Prefetch";
 
 function App() {
   return (
@@ -15,14 +18,24 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Public />} />
         <Route path="login" element={<Login />} />
-        <Route path="dash" element={<DashLayout />}>
-          <Route index element={<Welcome />} />
-          <Route path="notes">
-            <Route index element={<NoteList />} />
+
+        <Route element={<Prefetch />}>
+          <Route path="dash" element={<DashLayout />}>
+            <Route index element={<Welcome />} />
+
+            <Route path="users">
+              <Route index element={<UsersList />} />
+              <Route path=":id" element={<EditUser />} />
+              <Route path="new" element={<NewUserForm />} />
+            </Route>
+
+            <Route path="notes">
+              <Route index element={<NotesList />} />
+              <Route path=":id" element={<EditNote />} />
+              <Route path="new" element={<NewNote />} />
+            </Route>
           </Route>
-          <Route path="users">
-            <Route index element={<UserList />} />
-          </Route>
+          {/* End Dash */}
         </Route>
       </Route>
     </Routes>
