455fb08f5a9f232252fa6a4bca7edb212b3823ed
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-05-19 20:26:21 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+// ReactDOM.createRoot is the new way to render the app and we import ReactDOM from react-dom/client from React 18
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { store } from "./app/store";
+// Redux Provider provides the global states to the app
 import { Provider } from "react-redux";
 import App from "./App.jsx";
 import "./index.css";
