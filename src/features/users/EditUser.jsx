0a8346ae17ebc26912e04827adf5f3ed3863f7e7
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-05-27 07:28:49 +0700

    Update EditUser.jsx

--- src/features/users/EditUser.jsx
+++ src/features/users/EditUser.jsx
@@ -1,15 +1,28 @@
 import { useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { selectUserById } from "./usersApiSlice";
 import EditUserForm from "./EditUserForm";
+import { useGetUsersQuery } from "./usersApiSlice";
+import PulseLoader from "react-spinners/PulseLoader";
+import useTitle from "../../hooks/useTitle";
 
 const EditUser = () => {
+  // Custom hook to set the page title
+  useTitle("Meganote: Edit User");
+
+  // Get user id parameter out of the URL
   const { id } = useParams();
 
-  const user = useSelector(state => selectUserById(state, id));
+  const { user } = useGetUsersQuery("usersList", {
+    selectFromResult: ({ data }) => ({
+      user: data?.entities[id],
+    }),
+  });
 
-  const content = user ? <EditUserForm user={user} /> : <p>Loading...</p>;
+  if (!user) return <PulseLoader color={"#FFF"} />;
+
+  // Content to be rendered
+  const content = <EditUserForm user={user} />;
 
   return content;
 };
+
 export default EditUser;
