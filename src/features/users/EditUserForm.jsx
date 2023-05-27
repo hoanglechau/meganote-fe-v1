80274919daf260ae05b9439444ac899837da28ca
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-05-27 07:28:51 +0700

    Update EditUserForm.jsx

--- src/features/users/EditUserForm.jsx
+++ src/features/users/EditUserForm.jsx
@@ -5,13 +5,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSave, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import { ROLES } from "../../config/roles";
 
+// Regex patterns to check validity of username and password
 const USER_REGEX = /^[A-z]{3,20}$/;
 const PWD_REGEX = /^[A-z0-9!@#$%]{4,12}$/;
 
 const EditUserForm = ({ user }) => {
+  // Getting the 'updateUser' function and statuses after running this function from the custom hook
   const [updateUser, { isLoading, isSuccess, isError, error }] =
     useUpdateUserMutation();
 
+  // Also change the names of the default statuses received from this custom hook after running the 'deleteUser' function
   const [
     deleteUser,
     { isSuccess: isDelSuccess, isError: isDelError, error: delerror },
@@ -26,6 +29,7 @@ const EditUserForm = ({ user }) => {
   const [roles, setRoles] = useState(user.roles);
   const [active, setActive] = useState(user.active);
 
+  // Side effects to check the validity of username and password
   useEffect(() => {
     setValidUsername(USER_REGEX.test(username));
   }, [username]);
@@ -34,17 +38,19 @@ const EditUserForm = ({ user }) => {
     setValidPassword(PWD_REGEX.test(password));
   }, [password]);
 
+  // Side effect to reset the form after a successful update or delete
   useEffect(() => {
-    console.log(isSuccess);
     if (isSuccess || isDelSuccess) {
       setUsername("");
       setPassword("");
       setRoles([]);
       navigate("/dash/users");
     }
-  }, [isSuccess, isDelSuccess, navigate]);
+  }, [isSuccess, isDelSuccess, navigate]); // including 'navigate' here to avoid a warning
 
+  // Event handlers
   const onUsernameChanged = e => setUsername(e.target.value);
+
   const onPasswordChanged = e => setPassword(e.target.value);
 
   const onRolesChanged = e => {
@@ -52,10 +58,13 @@ const EditUserForm = ({ user }) => {
     setRoles(values);
   };
 
+  // Set the 'active' state to the opposite of its current value for the checkbox
   const onActiveChanged = () => setActive(prev => !prev);
 
   const onSaveUserClicked = async e => {
+    // Password is optional when updating, so we must update the user in two different ways
     if (password) {
+      // Run the function from the custom hook and passing in the new states as arguments
       await updateUser({ id: user.id, username, password, roles, active });
     } else {
       await updateUser({ id: user.id, username, roles, active });
@@ -63,9 +72,11 @@ const EditUserForm = ({ user }) => {
   };
 
   const onDeleteUserClicked = async () => {
+    // Run the action from the custom hook
     await deleteUser({ id: user.id });
   };
 
+  // Options for user roles drop-down
   const options = Object.values(ROLES).map(role => {
     return (
       <option key={role} value={role}>
@@ -76,6 +87,7 @@ const EditUserForm = ({ user }) => {
   });
 
   let canSave;
+  // Password is optional, so we must check for its validity only if it exists -> check in two different ways
   if (password) {
     canSave =
       [roles.length, validUsername, validPassword].every(Boolean) && !isLoading;
@@ -83,14 +95,18 @@ const EditUserForm = ({ user }) => {
     canSave = [roles.length, validUsername].every(Boolean) && !isLoading;
   }
 
+  // Show error messages and highlight incomplete/invalid fields
   const errClass = isError || isDelError ? "errmsg" : "offscreen";
   const validUserClass = !validUsername ? "form__input--incomplete" : "";
   const validPwdClass =
     password && !validPassword ? "form__input--incomplete" : "";
   const validRolesClass = !roles.length ? "form__input--incomplete" : "";
 
+  // This is based on whether we are using the update or delete mutation
+  // If error or delete error doesn't exist, then we don't want to display anything
   const errContent = (error?.data?.message || delerror?.data?.message) ?? "";
 
+  // Content to be rendered
   const content = (
     <>
       <p className={errClass}>{errContent}</p>
@@ -177,4 +193,5 @@ const EditUserForm = ({ user }) => {
 
   return content;
 };
+
 export default EditUserForm;
