6a1abadfc8da8265c87981cc408264b58d81d8f0
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-05-27 07:28:17 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,32 +11,58 @@ import NewUserForm from "./features/users/NewUserForm";
 import EditNote from "./features/notes/EditNote";
 import NewNote from "./features/notes/NewNote";
 import Prefetch from "./features/auth/Prefetch";
+import PersistLogin from "./features/auth/PersistLogin";
+import RequireAuth from "./features/auth/RequireAuth";
+import { ROLES } from "./config/roles";
+import useTitle from "./hooks/useTitle";
 
 function App() {
+  // Custom hook to set the page title
+  useTitle("Meganote");
+
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
+        {/* Public routes */}
         <Route index element={<Public />} />
         <Route path="login" element={<Login />} />
 
-        <Route element={<Prefetch />}>
-          <Route path="dash" element={<DashLayout />}>
-            <Route index element={<Welcome />} />
+        {/* Protected Routes */}
+        <Route element={<PersistLogin />}>
+          {/* All roles (logged in users) are allowed to access the wrapped routes */}
+          <Route
+            element={<RequireAuth allowedRoles={[...Object.values(ROLES)]} />}
+          >
+            {/* If someone is not authorized, we're not going to prefetch the data, so we wrap 'RequireAuth' around 'Prefect' */}
+            <Route element={<Prefetch />}>
+              {/* Dash */}
+              <Route path="dash" element={<DashLayout />}>
+                <Route index element={<Welcome />} />
 
-            <Route path="users">
-              <Route index element={<UsersList />} />
-              <Route path=":id" element={<EditUser />} />
-              <Route path="new" element={<NewUserForm />} />
-            </Route>
+                {/* Only managers and admins are allowed to access the users route */}
+                <Route
+                  element={
+                    <RequireAuth allowedRoles={[ROLES.Manager, ROLES.Admin]} />
+                  }
+                >
+                  <Route path="users">
+                    <Route index element={<UsersList />} />
+                    <Route path=":id" element={<EditUser />} />
+                    <Route path="new" element={<NewUserForm />} />
+                  </Route>
+                </Route>
 
-            <Route path="notes">
-              <Route index element={<NotesList />} />
-              <Route path=":id" element={<EditNote />} />
-              <Route path="new" element={<NewNote />} />
+                <Route path="notes">
+                  <Route index element={<NotesList />} />
+                  <Route path=":id" element={<EditNote />} />
+                  <Route path="new" element={<NewNote />} />
+                </Route>
+              </Route>
+              {/* End Dash */}
             </Route>
           </Route>
-          {/* End Dash */}
         </Route>
+        {/* End Protected Routes */}
       </Route>
     </Routes>
   );
