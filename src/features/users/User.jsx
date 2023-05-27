01a78b252d539ca49b07e33360415a8bc5bb0c82
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-05-27 07:28:55 +0700

    Update User.jsx

--- src/features/users/User.jsx
+++ src/features/users/User.jsx
@@ -1,20 +1,29 @@
+// Use FontAwesome to create icons
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
-
-import { useSelector } from "react-redux";
-import { selectUserById } from "./usersApiSlice";
+import { useGetUsersQuery } from "./usersApiSlice";
+import { memo } from "react";
 
 const User = ({ userId }) => {
-  const user = useSelector(state => selectUserById(state, userId));
+  const { user } = useGetUsersQuery("usersList", {
+    selectFromResult: ({ data }) => ({
+      // the 'entities' property is from the 'usersAdapter' in usersApiSlice, which contains all the users
+      user: data?.entities[userId],
+    }),
+  });
 
   const navigate = useNavigate();
 
+  // If the user exists
   if (user) {
+    // Navigate to the edit user page when the edit button is clicked
     const handleEdit = () => navigate(`/dash/users/${userId}`);
 
+    // Improve formatting to make it easier to read
     const userRolesString = user.roles.toString().replaceAll(",", ", ");
 
+    // If the user is inactive, add the 'table__cell--inactive' class to the cell
     const cellStatus = user.active ? "" : "table__cell--inactive";
 
     return (
@@ -28,6 +37,11 @@ const User = ({ userId }) => {
         </td>
       </tr>
     );
-  } else return null;
+  } else return null; // if there's no user
 };
-export default User;
+
+// React optimization
+// Memoize the component -> Only re-render when the props change -> This component will only re-render when the data is changed
+const memoizedUser = memo(User);
+
+export default memoizedUser;
