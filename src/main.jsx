62c54276ca9a3b9c424233db40529fb3327e4db1
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-05-27 07:29:44 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { store } from "./app/store";
 // Redux Provider provides the global states to the app
 import { Provider } from "react-redux";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 import App from "./App.jsx";
 import "./index.css";
+
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
